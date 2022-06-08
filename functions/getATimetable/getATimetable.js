1eaf8305c49d2b0bf4c6bd3583171c9a2044c94a
Markus <markus.mork@iths.se>
2022-06-08 12:01:16 +0200

    Fix timetable request

--- functions/getATimetable/getATimetable.js
+++ functions/getATimetable/getATimetable.js
@@ -5,16 +5,27 @@ const API_KEY = process.env.API_KEY
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 //access_key="123"&query=1234,1234&vårtAPI
 const handler = async (event) => {
-  const url = "https://api.resrobot.se/v2.1/departureBoard?id=740000002&format=json"
+
+  const timeNow = new Date()
+  // timeNow = timeNow.toLocaleTimeString()
+  timeNow.setMinutes(timeNow.getMinutes() + 10);
+  // console.log(timeNow.toLocaleTimeString())
+  let current = timeNow.getHours() + ':' + timeNow.getMinutes();
+  console.log(current)
+  const url = `https://api.resrobot.se/v2.1/departureBoard?time=${current}&maxJourneys=10&id=740004046&format=json`
   try{
+
     const response = await axios.get(`${url}&accessId=${API_KEY}`)
+    // let departures = response.data.Departure.slice(0, 3)
+    // console.log('DEPARTURES ÄR: ' , departures)
+
     // console.log('Response från getATimetable är ' , response)
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
     }
   }catch(error){
-    console.log('Error från getATimetable är ' , error )
+    // console.log('Error från getATimetable är ' , error.message )
     return {
       statusCode: 500,
       body: error.message
--- src/components/Traffic.jsx
+++ src/components/Traffic.jsx
@@ -1,20 +1,15 @@
-import { getATimetable, getStopId } from "../API/index.js"
+import { getATimetable } from "../API/index.js"
 
 const Traffic = () => {
     async function getTheBus(){
         const response = await getATimetable()
-        console.log('getTheBus är: ' , response)
     }
     getTheBus()
-    async function getStop(){
-        const response = await getStopId()
-        console.log('Get stop är: ' , response)
-    }
 
     return (
         <div>
             <p>Traffikinfo</p>
-            <button onClick={getStop}>Get stop</button>
+            {/* <button onClick={getStop}>Get stop</button> */}
         </div>
     )
 }
