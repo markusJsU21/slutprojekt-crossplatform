5cbb2dff948bc2b4897d5c099c15deeab6881eee
Markus <markus.mork@iths.se>
2022-06-09 14:52:34 +0200

    Slim rendering and some styling

--- src/components/Traffic.jsx
+++ src/components/Traffic.jsx
@@ -2,28 +2,47 @@ import { getATimetable } from "../API/index.js"
 import {useState, useEffect} from 'react'
 
 const ListItem = ({line, time, destination}) => {
-    return <li>Linje: {line} Avgår: {time} Mot: {destination}</li>
+    return <li> <p>{line}</p> <p>{destination}</p>  <p>{time}</p></li>
   }
+// const TableRow = ({line, time, destination}) => {
+//     return (<tr>
+//     <td>{line}</td>
+//     <td>{destination}</td>
+//     <td>{time}</td>
+//   </tr>)
+//   }
 
 const Traffic = () => {
     const [table, setTable] = useState([])
 
     useEffect(()=>{
         getTheBus()
+        setInterval(()=>{
+            console.log('Calling get the bus')
+            getTheBus()
+        },3000 * 60)
     },[])
     async function getTheBus(){
         const response = await getATimetable()
         const tenNext = response.Departure.slice(0, 10)
-
-        console.log(typeof tenNext)
+        for(let departure of tenNext){
+            if(departure.direction.includes('(')){
+               departure.direction = departure.direction.replace(/ *\([^)]*\) */g, "");
+            }
+            departure.time = departure.time.slice(0, 5)
+        }
         setTable(tenNext)
-        console.log(tenNext)
-
     }
+
 
     return (
         <div>
             <p>Avgångar från Liljeholmen om 10 minuter</p>
+            <div className="table-header">
+                <p>Linje</p>
+                <p>Mot</p>
+                <p>Avgår</p>
+            </div>
             <ul>
         {/* {list} */}
         {/* for(let tenDepartures of departures){
@@ -33,6 +52,16 @@ const Traffic = () => {
 
         {table.map(departure => (<ListItem line={departure.ProductAtStop.line} time={departure.time} destination={departure.direction} key={departure.Product.matchId} />) )}
       </ul>
+        {/* <table>
+
+            <tr  className="table-header">
+                <th colspan="1">Linje</th>
+                <th colspan="2">Mot</th>
+                <th colspan="3">Avgår</th>
+            </tr>
+
+        {table.map(departure => (<TableRow line={departure.ProductAtStop.line} time={departure.time} destination={departure.direction} key={departure.Product.matchId} />) )}
+        </table> */}
             {/* <button onClick={getStop}>Get stop</button> */}
         </div>
     )
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,10 +1,12 @@
 import Clock from '../components/Clock'
 import Traffic from '../components/Traffic'
+import './Dashboard.css';
 // import Weather from '../components/Weather'
 
 const Dashboard = () => {
     return(
         <div>
+            <h1>It-Högskolan</h1>
             <Clock />
             <Traffic />
         </div>
