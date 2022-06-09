7eafc144cf339d4df5e6890ef7031327efedc6dd
Markus <markus.mork@iths.se>
2022-06-09 11:18:16 +0200

    Add conditional netlify path

--- src/API/index.js
+++ src/API/index.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 // axios.baseURL = 'http://localhost:8888/.netlify/functions'
 
 export async function getATimetable(){
-
-    const response = await axios.get(`http://localhost:8888/.netlify/functions/getATimetable`)
+    if(process.env.NODE_ENV = 'production'){
+        const response = await axios.get(`https://stunning-treacle-66df98.netlify.app/.netlify/functions/getATimetable`)
+        return response.data
+    }
+    else{
+        const response = await axios.get(`http://localhost:8888/.netlify/functions/getATimetable`)
+        return response.data
+    }
 //    const response = await axios.get(`https://api.resrobot.se/v2.1/departureBoard?id=740000002&format=json&accessId=${API_KEY}`)
-    return response.data
 }
 // export async function getStopId(){
 //     const response = await axios.get(`http://localhost:8888/.netlify/functions/getStopId`)
