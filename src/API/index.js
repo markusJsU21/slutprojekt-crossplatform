10700e773ffcd83a6b1326984677ea325d64a5c2
Markus <markus.mork@iths.se>
2022-06-09 11:21:55 +0200

    testing

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // axios.baseURL = 'http://localhost:8888/.netlify/functions'
 
 export async function getATimetable(){
-    if(process.env.NODE_ENV = 'production'){
+    if(process.env.NODE_ENV === 'production'){
         const response = await axios.get(`https://stunning-treacle-66df98.netlify.app/.netlify/functions/getATimetable`)
         return response.data
     }
