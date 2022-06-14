35e48c212c44b1468368b6e1788bd86a041c6153
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-06-14 14:22:16 +0200

    adding icons for weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react'
 import {getWeather} from '../API/index'
 import './Weather.css'
+import Sunny from '../assets/sunny_regular.svg'
+import MoonRainNight from '../assets/night_raining_icon.svg'
+import CloudyLightning from '../assets/cloudy_lightning_icon.svg'
+import CloudySunny from '../assets/cloudy_sunny_icon.svg'
+import CloudRainSun from '../assets/cloud_raining_sun_icon.svg'
+import Snow from '../assets/snow_icon.svg'
 
 
 
-const ListItem = ({hour, temperature}) => {
-    return <li> <p>{hour}</p> <p>{temperature} °C</p></li>
+
+const ListItem = ({hour, temperature, symbol}) => {
+    return <li> <p>{hour}</p><img  className="weather-symbol "src={symbol} alt="weather symbol" /><p>{temperature} °C</p></li>
   }
 
 const Weather = () => {
@@ -21,6 +28,13 @@ const Weather = () => {
         const forecasts = response.timeSeries.slice(0, 5)
         for(let forecast of forecasts){
            forecast.validTime = forecast.validTime.slice(11, 16)
+           if(forecast.parameters[18].values < 3){
+               forecast.img = Sunny
+           } else if (forecast.parameters[18].values < 6) {
+           forecast.img = CloudySunny
+        } else if (forecast.parameters[18].values < 11)
+        forecast.img = CloudRainSun
+    }
            for(let param of forecast.parameters){
             if (param.name==='t' && param.unit === 'Cel') {
                 forecast.temperature = param.values
@@ -40,6 +54,7 @@ const Weather = () => {
                 <ul>
                     {table.map(forecast => (<ListItem
                     hour={forecast.validTime}
+                    symbol={forecast.img}
                     temperature={forecast.temperature}
                     key={forecast.validTime} />) )}
                 </ul>
---
+++ src/components/images.jsx
@@ -0,0 +1,8 @@
+import sunny from '../assets/sun-regular.svg'
+import moonRainNight from '../assets/night_raining_icon.svg'
+import cloudyLightning from '../assets/cloudy_lightning_icon.svg'
+import cloudySunny from '../assets/cloudy_sunny_icon.svg'
+import cloudRainSun from '../assets/cloud_raining_sun_icon.svg'
+import snow from '../assets/snow_icon.svg'
+
+export {sunny, moonRainNight, cloudRainSun, cloudySunny, cloudyLightning, snow}
