b537051886f167411ef1441c17bd1b704e608405
Markus <markus.mork@iths.se>
2022-06-15 14:31:39 +0200

    Add offline message to Dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -10,6 +10,9 @@ const Dashboard = () => {
         <div>
             <div className="component-container">
                 <h1>It-Högskolan</h1>
+               {navigator.OnLine === false &&
+               <p>Your are offline and will not receive the latest information</p>
+               }
                     <Clock className="clock" />
                     <Weather className="weather"/>
                     <Traffic className="traffic"/>
