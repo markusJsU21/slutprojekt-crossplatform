2b20a54d57ef3dbaed4525b991cfdc6bbc5f3624
Markus <markus.mork@iths.se>
2022-06-15 13:46:53 +0200

    Delete server

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({path: __dirname + '/../.env'})
-
-module.exports = {
-  publicKey: process.env.publicKey,
-  privateKey: process.env.privateKey,
-  PORT: process.env.PORT || 8000
-}
