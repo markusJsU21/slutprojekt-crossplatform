d0dcbff3ed946c6b3aef24a130cc54b44cdb1717
Markus <markus.mork@iths.se>
2022-06-15 09:20:01 +0200

    Fix weather icon logic

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 import {getWeather} from '../API/index'
 import './Weather.css'
-import Sunny from '../assets/sunny_regular.svg'
+// import Sunny from '../assets/sunny_regular.svg'
+import icons from './images'
 // import MoonRainNight from '../assets/night_raining_icon.svg'
 // import CloudyLightning from '../assets/cloudy_lightning_icon.svg'
-import CloudySunny from '../assets/cloudy_sunny_icon.svg'
-import CloudRainSun from '../assets/cloud_raining_sun_icon.svg'
+// import CloudySunny from '../assets/cloudy_sunny_icon.svg'
+// import CloudRainSun from '../assets/cloud_raining_sun_icon.svg'
 // import Snow from '../assets/snow_icon.svg'
 
 
@@ -28,12 +29,7 @@ const Weather = () => {
         const forecasts = response.timeSeries.slice(0, 5)
         for(let forecast of forecasts){
            forecast.validTime = forecast.validTime.slice(11, 16)
-           if(forecast.parameters[18].values < 3){
-               forecast.img = Sunny
-           } else if (forecast.parameters[18].values < 6) {
-           forecast.img = CloudySunny
-        } else if (forecast.parameters[18].values < 11)
-        forecast.img = CloudRainSun
+            forecast.img = icons[forecast.parameters[18].values]
            for(let param of forecast.parameters){
             if (param.name==='t' && param.unit === 'Cel') {
                 forecast.temperature = param.values
--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,4 +1,4 @@
-import sunny from '../assets/sun-regular.svg'
+import sunny from '../assets/sunny_regular.svg'
 import moonRainNight from '../assets/night_raining_icon.svg'
 import cloudyLightning from '../assets/cloudy_lightning_icon.svg'
 import cloudySunny from '../assets/cloudy_sunny_icon.svg'
@@ -14,8 +14,27 @@ const icons = {
     4: cloudySunny,
     5: cloudySunny,
     6: cloudySunny,
-
-        , moonRainNight, cloudRainSun, cloudySunny, cloudyLightning, snow
+    7: moonRainNight,
+    8: moonRainNight,
+    9: cloudRainSun,
+    10: cloudRainSun,
+    11: cloudRainSun,
+    12: cloudRainSun,
+    13: cloudySunny,
+    14: cloudySunny,
+    15: cloudySunny,
+    16: cloudySunny,
+    17: cloudySunny,
+    18: cloudySunny,
+    19: cloudyLightning,
+    20: cloudyLightning,
+    21: cloudyLightning,
+    22: cloudyLightning,
+    23: cloudyLightning,
+    24: snow,
+    25: snow,
+    26: snow,
+    27: snow
 }
 
 export default icons
