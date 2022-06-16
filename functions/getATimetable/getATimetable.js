9f7e530dd8d13e2d45aa12580dbf725d49a3c3ab
Markus <markus.mork@iths.se>
2022-06-16 11:23:34 +0200

    Try fix offline status text

--- functions/getATimetable/getATimetable.js
+++ functions/getATimetable/getATimetable.js
@@ -3,15 +3,7 @@ const axios = require('axios')
 const API_KEY = process.env.API_KEY
 const handler = async (event) => {
 
-  // const timeNow = new Date()
   const current = event.queryStringParameters.current
-  // timeNow.setMinutes(timeNow.getMinutes() + 10)
-  // let hours = timeNow.getHours().toString().padStart(2, '0')
-  // const minutes = timeNow.getMinutes().toString().padStart(2, '0')
-  // let current = hours + ':' + minutes
-
-
-  //Ta bara duration = 30 min
   const url = `https://api.resrobot.se/v2.1/departureBoard?time=${current}&maxJourneys=10&id=740004046&format=json`
   try{
 
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -2,15 +2,32 @@ import Clock from '../components/Clock'
 import Traffic from '../components/Traffic'
 import Weather from '../components/Weather';
 import logo from '../assets/ITHS_logo.png'
+import {useState} from 'react'
 
 import styles from './Dashboard.module.css';
 // import Weather from '../components/Weather'
 
 const Dashboard = () => {
+    const [onlineStatus, setOnlineStatus] = useState(navigator.onLine)
+    function handleConnectionChange(event){
+        if(event.type == "offline"){
+            setOnlineStatus(!onlineStatus)
+            console.log("You lost connection.");
+        }
+        if(event.type == "online"){
+            setOnlineStatus(!onlineStatus)
+            console.log("You are now back online.");
+        }
+
+        console.log(new Date(event.timeStamp));
+    }
+    window.addEventListener('online', handleConnectionChange);
+    window.addEventListener('offline', handleConnectionChange);
+
     return(
         <div>
                 <img src={logo} alt="ITHS logo" className={styles.ithsLogo}/>
-               {navigator.onLine === false &&
+               {onlineStatus === false &&
                <p>Your are offline and will not receive the latest information</p>
                }
             <div className="component-container">
